ac3c671ca39655689f5097c4b29dc34d4b6010ae
virajjiwane <vjiwane27@gmail.com>
2025-10-29 16:09:41 +0530

    UI

---
+++ src/app/app.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { IntersectionDirective } from './directives/intersection';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.html',
+  styleUrl: './app.scss',
+  standalone: true,
+  imports: [IntersectionDirective],
+})
+export class App {
+
+  handleViewChange(event: any, animationClasses: string) {
+    if (event.isIntersecting) {
+      // Add the animation class when the element is in view
+      event.target.classList.add(...animationClasses.split(' '));
+    }
+  }
+
+}
