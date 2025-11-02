4f1025d2b2e79ed2d8b208ec8381406e9737f375
virajjiwane <vjiwane27@gmail.com>
2025-11-02 06:21:31 +0530

    contact form working

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,18 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, signal, ViewChild } from '@angular/core';
 import { IntersectionDirective } from './directives/intersection';
+import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
+import { FormGroup, FormControl, ReactiveFormsModule, Validators, NgForm } from '@angular/forms';
+import { JsonPipe, NgClass } from '@angular/common';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.html',
     styleUrl: './app.scss',
     standalone: true,
-    imports: [IntersectionDirective],
+    imports: [IntersectionDirective, ReactiveFormsModule, NgClass],
 })
 export class App {
+    emailSent = signal(false);
+    emailForm = new FormGroup(
+        {
+            name: new FormControl('', Validators.required),
+            email: new FormControl('', [Validators.required, Validators.email]),
+            title: new FormControl('', Validators.required),
+            message: new FormControl('', Validators.required),
+        },
+        {
+            updateOn: 'change',
+        },
+    );
+
     handleViewChange(event: any, animationClasses: string) {
         if (event.isIntersecting) {
             // Add the animation class when the element is in view
             event.target.classList.add(...animationClasses.split(' '));
         }
+    }
+
+    public sendEmail() {
+        emailjs
+            .send('service_p6epjnl', 'template_r907mj4', {
+                ...this.emailForm.value,
+            }, {
+                publicKey: '9z5tnKtdB_iK5PmTE',
+            })
+            .then(
+                () => {
+                    console.log('SUCCESS!');
+                    this.emailSent.set(true);
+                    this.emailForm.reset();
+                },
+                (error) => {
+                    console.log('FAILED...', error, (error as EmailJSResponseStatus).text);
+                    // Display toast notification for failure
+                    alert('Failed to send the message. I probably ran out of credits to send mails. Please contact me directly at vjiwane27@gmail.com.');
+                },
+            );
     }
 }
